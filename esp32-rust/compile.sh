14af2c2a8d92ad780f10ac07d7129b180459be64
Juraj Michálek <juraj.michalek@espressif.com>
2022-03-07 15:23:29 +0100

    ci: build esp32-c3

--- esp32-rust/compile.sh
+++ esp32-rust/compile.sh
@@ -1,6 +1,41 @@
 #!/bin/sh
 
-cd rust-project
-cat build-in/main.rs > examples/ledc-simple.rs && \
-cargo +esp build --example ledc-simple --release --target xtensa-esp32-espidf && \
-python3 -m esptool --chip esp32 elf2image --flash_size 4MB target/xtensa-esp32-espidf/release/examples/ledc-simple -o build-out/project.bin
+set -e
+
+case ${WOKWI_MCU} in
+    "esp32")
+      TOOLCHAIN="+esp"
+      TARGET="xtensa-esp32-espidf"
+      ;;
+    "esp32-s2")
+      TOOLCHAIN="+esp"
+      TARGET="xtensa-esp32s2-espidf"
+      ;;
+    "esp32-s3")
+      TOOLCHAIN="+esp"
+      TARGET="xtensa-esp32s3-espidf"
+      ;;
+    "esp32-c3")
+      TOOLCHAIN=""
+      TARGET="riscv32imc-esp-espidf"
+      ;;
+    *) # unknown
+      echo "Environment variable WOKWI_MCU not set."
+      echo "Available values esp32, esp32-s2, esp32-c3"
+      exit 1
+      ;;
+esac
+
+echo "Configuration of ${WOKWI_MCU}"
+echo " - TARGET    = ${TARGET}"
+echo " - TOOLCHAIN = ${TOOLCHAIN}"
+
+cd ~/rust-project
+mkdir -p build-out
+
+if [ -f build-in/main.rs ]; then
+  cat build-in/main.rs > examples/ledc-simple.rs
+fi
+
+cargo ${TOOLCHAIN} build --example ledc-simple --release --target ${TARGET}
+python3 -m esptool --chip ${WOKWI_MCU} elf2image --flash_size 4MB target/${TARGET}/release/examples/ledc-simple -o build-out/project.bin
