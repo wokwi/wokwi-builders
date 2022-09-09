db8ed67fe804a1dfab6dd789c0aebea0af635849
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-09-09 12:27:01 +0200

    feat: 🔧 Build `display.sr`

--- rust-std-esp32c3/compile.sh
+++ rust-std-esp32c3/compile.sh
@@ -6,6 +6,9 @@ cd rust-project
 if [ -f ${HOME}/build-in/main.rs ]; then
     cat ${HOME}/build-in/main.rs > src/main.rs
 fi
+if [ -f ${HOME}/build-in/display.rs ]; then
+    cat ${HOME}/build-in/display.rs > src/display.rs
+fi
 cargo build --offline --release
 python3 -m esptool --chip ${WOKWI_MCU} elf2image --flash_size 4MB target/riscv32imc-esp-espidf/release/rust-project -o ${HOME}/build-out/project.bin
 cp target/riscv32imc-esp-espidf/release/rust-project ${HOME}/build-out/project.elf
