84ed29d8ea46c443b7f1bcf38c2e46c93a45c5dc
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-11-02 16:40:16 +0100

    feat: ✨ Add no-std builder and rename general builders

---
+++ esp-rust-std/compile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+. /home/esp/export-esp.sh
+export CARGO_INCREMENTAL=0
+export RUSTC_WRAPPER=$(which sccache)
+
+cd rust-project
+mkdir -p output
+
+if [ -f ${HOME}/build-in/main.rs ]; then
+    cat ${HOME}/build-in/main.rs >src/main.rs
+fi
+
+if [ -f ${HOME}/build-in/Cargo.rs ]; then
+    cat ${HOME}/build-in/Cargo.rs >Cargo.toml
+fi
+
+cargo build --release --out-dir output -Z unstable-options
+python3 -m esptool --chip ${WOKWI_MCU} elf2image --flash_size 4MB output/rust-project -o ${HOME}/build-out/project.bin
+cp output/release/rust-project ${HOME}/build-out/project.elf
