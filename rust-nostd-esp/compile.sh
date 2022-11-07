8def3662e35d04fde37670fcaa4e2b1ac3ed0965
Sergio Gasquez <sergio.gasquez@gmail.com>
2022-11-07 12:29:15 +0100

    refactor: 🎨 Use target arg to build for WOKWI_MCU

--- rust-nostd-esp/compile.sh
+++ rust-nostd-esp/compile.sh
@@ -16,6 +16,12 @@ if [ -f ${HOME}/build-in/Cargo.toml ]; then
     cat ${HOME}/build-in/Cargo.toml >Cargo.toml
 fi
 
-cargo build --release --out-dir output -Z unstable-options
+if [ "$WOKWI_MCU" == "esp32c3" ]; then
+    TARGET=riscv32imac-unknown-none-elf
+else
+    TARGET=xtensa-${WOKWI_MCU}-none-elf
+fi
+
+cargo build --release --target ${TARGET} --out-dir output -Z unstable-options
 python3 -m esptool --chip ${WOKWI_MCU} elf2image --flash_size 4MB output/rust-project -o ${HOME}/build-out/project.bin
 cp output/release/rust-project ${HOME}/build-out/project.elf
