7dd3b64811204931cf1383326e9fccaee8bba281
Matheus Catarino <matheus-catarino@hotmail.com>
2022-11-18 17:58:09 -0300

    wokwi_chip_ii - zig wrapper added

--- zig-wasm/src/main.zig
+++ zig-wasm/src/main.zig
@@ -1,24 +1,19 @@
 const std = @import("std");
+const wokwi = @import("wokwi_chip_ii.zig");
 
 // TODO
-const PinId = opaque {};
 const Chip = extern struct {
-    pin_in: PinId,
-    pin_out: PinId,
+    pin_in: wokwi.PinId,
+    pin_out: wokwi.PinId,
 };
-// pub const INPUT = 0;
-// pub const OUTPUT = 0;
-
-pub extern fn pinInit(info: [*:0]const u8, io: u32) callconv(.C) PinId;
 
 pub export fn chipInit() callconv(.C) void {
     std.debug.print("Hello {s}!\n", .{"Zig"});
-
-    // const chip = Chip{
-    //     .pin_in = pinInit("IN", INPUT),
-    //     .pin_out = pinInit("OUT", OUTPUT),
-    // };
-    // _ = chip;
+    //    const chip = Chip{
+    //        .pin_in = wokwi.pinInit("IN", wokwi.INPUT),
+    //        .pin_out = wokwi.pinInit("OUT", wokwi.OUTPUT),
+    //    };
+    //    _ = chip;
 }
 
 test "simple test" {
