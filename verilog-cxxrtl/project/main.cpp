eeabb1b8cd555608f81e1c0e0622581af5ff940c
Uri Shaked <uri@urishaked.com>
2022-12-13 10:30:07 +0200

    fix(verilog-cxxrtl): constant pins not working

--- verilog-cxxrtl/project/main.cpp
+++ verilog-cxxrtl/project/main.cpp
@@ -19,35 +19,49 @@ typedef struct chip_state_t {
   struct chip_pin_t *first_pin;
 } chip_state_t;
 
-static void chip_input_pin_changed(void *state, wokwi::pin_t pin, uint32_t value) {
-  chip_pin_t *ctx = (chip_pin_t *)state;
-  auto design = ctx->chip->design;
-  if (ctx->verilog_pin.width == 1) {
-    ctx->verilog_pin.next[0] = value;
+static void update_input_pin(cxxrtl::debug_item verilog_pin, wokwi::pin_t wokwi_pin, uint8_t bit_index, uint32_t value) {
+  if (verilog_pin.width == 1) {
+    verilog_pin.next[0] = value;
   } else {
-    uint32_t v = ctx->verilog_pin.curr[0];
+    uint32_t v = verilog_pin.curr[0];
     if (value) {
-      v |= (1 << ctx->bit_index);
+      v |= (1 << bit_index);
     } else {
-      v &= ~(1 << ctx->bit_index);
+      v &= ~(1 << bit_index);
     }
-    ctx->verilog_pin.next[0] = v;
+    verilog_pin.next[0] = v;
   }
-  design->step();
+}
 
-  for (auto pin = ctx->chip->first_pin; pin; pin = pin->next) {
+static void update_output_pin(cxxrtl::debug_item verilog_pin, wokwi::pin_t wokwi_pin) {
+  if (verilog_pin.width == 1) {
+    wokwi::pin_write(wokwi_pin, verilog_pin.curr[0] ? wokwi::HIGH : wokwi::LOW);
+  } else {
+    for (uint32_t i = 0; i < verilog_pin.width; i++) {
+      wokwi::pin_write(wokwi_pin, verilog_pin.curr[i]);
+    }
+  }
+}
+
+static void inline update_output_pins(chip_state_t *chip) {
+  for (auto pin = chip->first_pin; pin; pin = pin->next) {
     auto verilog_pin = pin->verilog_pin;
     wokwi::pin_t wokwi_pin = pin->wokwi_pin;
     if (verilog_pin.flags & cxxrtl::debug_item::OUTPUT) {
-      if (verilog_pin.width == 1) {
-        wokwi::pin_write(wokwi_pin, verilog_pin.curr[0] ? wokwi::HIGH : wokwi::LOW);
-      } else {
-        for (uint32_t i = 0; i < verilog_pin.width; i++) {
-          wokwi::pin_write(wokwi_pin, verilog_pin.curr[i]);
-        }
-      }
+      update_output_pin(verilog_pin, wokwi_pin);
     }
   }
+}
+
+static void chip_input_pin_changed(void *state, wokwi::pin_t pin, uint32_t value) {
+  chip_pin_t *ctx = (chip_pin_t *)state;
+
+  update_input_pin(ctx->verilog_pin, pin, ctx->bit_index, value);
+
+  auto design = ctx->chip->design;
+  design->step();
+
+  update_output_pins(ctx->chip);
 }
 
 static chip_pin_t *create_pin(chip_state_t *chip) {
@@ -81,12 +95,20 @@ extern "C" void chip_init(void) {
           .edge = wokwi::BOTH,
           .pin_change = chip_input_pin_changed,
         };
+        uint32_t value = wokwi::pin_read(pin->wokwi_pin);
+        update_input_pin(part, pin->wokwi_pin, 0, value);
         wokwi::pin_watch(pin->wokwi_pin, &config);
       } else if (part.flags & cxxrtl::debug_item::OUTPUT) {
         chip_pin_t *pin = create_pin(chip);
         pin->wokwi_pin = wokwi::pin_init(name, wokwi::OUTPUT);
         pin->verilog_pin = part;
+      } else if (part.flags & CXXRTL_DRIVEN_COMB && part.type == CXXRTL_VALUE ) { // Constant pin
+        wokwi::pin_t pin = wokwi::pin_init(name, wokwi::OUTPUT);
+        update_output_pin(part, pin);
       }
     }
   }
+
+  chip->design->step();
+  update_output_pins(chip);
 }
