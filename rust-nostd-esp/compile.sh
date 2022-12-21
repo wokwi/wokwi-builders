fb4e96f628d0042b09caa5b86842195fa2d1cac1
Sergio Gasquez Arcos <sergio.gasquez@gmail.com>
2022-12-21 10:57:17 +0100

    feat(rust): update builders and add audit (#25)
    * feat: ✨ Add cargo-audit
    
    * chore: 🎨 Update image
    
    * chore: 🎨 Update images
    
    * chore: ⬆️ Upgrade dependencies
    
    * chore: 🎨 Update comment
    
    * chore: 🎨 Use hardcoded version of docker image

--- rust-nostd-esp/compile.sh
+++ rust-nostd-esp/compile.sh
@@ -6,22 +6,22 @@ export CARGO_INCREMENTAL=0
 export RUSTC_WRAPPER=$(which sccache)
 
 case ${WOKWI_MCU} in
-    "esp32")
-        PROJECT_NAME="rust-project-esp32"
-        ;;
-    "esp32-c3")
-        PROJECT_NAME="rust-project-esp32c3"
-        ;;
-    "esp32-s2")
-        PROJECT_NAME="rust-project-esp32s2"
-        ;;
-    "esp32-s3")
-        PROJECT_NAME="rust-project-esp32s3"
-        ;;
-    *)
-        echo "Missing or invalid WOKWI_MCU environment variable"
-        exit 1
-        ;;
+"esp32")
+    PROJECT_NAME="rust-project-esp32"
+    ;;
+"esp32-c3")
+    PROJECT_NAME="rust-project-esp32c3"
+    ;;
+"esp32-s2")
+    PROJECT_NAME="rust-project-esp32s2"
+    ;;
+"esp32-s3")
+    PROJECT_NAME="rust-project-esp32s3"
+    ;;
+*)
+    echo "Missing or invalid WOKWI_MCU environment variable"
+    exit 1
+    ;;
 esac
 
 cd ${PROJECT_NAME}
@@ -37,6 +37,7 @@ fi
 
 PROJECT_ROOT="${HOME}/${PROJECT_NAME}"
 PROJECT_NAME_UNDERSCORE=${PROJECT_NAME//-/_}
+cargo audit
 cargo build --release --out-dir output -Z unstable-options
 python3 -m esptool --chip ${WOKWI_MCU} elf2image --flash_size 4MB ${PROJECT_ROOT}/output/${PROJECT_NAME_UNDERSCORE} -o ${HOME}/build-out/project.bin
 cp output/${PROJECT_NAME_UNDERSCORE} ${HOME}/build-out/project.elf
