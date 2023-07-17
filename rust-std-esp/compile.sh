c6387346465ec647ccfaf330eb04b252fc6262d1
Uri Shaked <uri@urishaked.com>
2023-07-17 16:39:43 +0300

    perf(rust-std-esp): remove sccache
    also skip precompiling the template projects to reducce container image size

--- rust-std-esp/compile.sh
+++ rust-std-esp/compile.sh
@@ -3,8 +3,6 @@
 set -e
 . /home/esp/export-esp.sh
 export ESP_IDF_TOOLS_INSTALL_DIR=global
-export CARGO_INCREMENTAL=0
-export RUSTC_WRAPPER=$(which sccache)
 
 case ${WOKWI_MCU} in
 "esp32")
@@ -37,9 +35,7 @@ if [ -f ${HOME}/build-in/Cargo.toml ]; then
     cp ${HOME}/build-in/Cargo.toml Cargo.toml
     sed -i 's/^[[:space:]]*name[[:space:]]*=[[:space:]]*["'"'"']\([^"'"'"']*\)["'"'"']\([[:space:]]*\)$/\nname = "'${PROJECT_NAME}'"/' Cargo.toml
 fi
-# RUSTSEC-2020-0071 comes from esp-idf-hal dependency on time `0.1.45`
-# Once that time is >=`0.2.23` this can be removed
-cargo audit --ignore RUSTSEC-2020-0071
+
 cargo build --release --out-dir output -Z unstable-options
 python3 -m esptool --chip ${WOKWI_MCU} elf2image --flash_size 4MB ${PROJECT_ROOT}/output/${PROJECT_NAME} -o ${HOME}/build-out/project.bin
 cp output/${PROJECT_NAME} ${HOME}/build-out/project.elf
