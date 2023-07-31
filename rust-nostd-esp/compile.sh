0e1afcbb9d35b88e78c2366ffa76161a40bf3159
Uri Shaked <uri@urishaked.com>
2023-07-31 15:19:02 +0300

    fix(rust-nostd-esp): reduce compiler output verbosity

--- rust-nostd-esp/compile.sh
+++ rust-nostd-esp/compile.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-uptime
-
 set -e
 . /home/esp/export-esp.sh
 
@@ -39,6 +37,6 @@ if [ -f ${HOME}/build-in/Cargo.toml ]; then
 fi
 
 cargo audit
-cargo build --release --out-dir output -Z unstable-options --verbose
+cargo build --release --out-dir output -Z unstable-options
 python3 -m esptool --chip ${WOKWI_MCU} elf2image --flash_size 4MB ${PROJECT_ROOT}/output/${PROJECT_NAME_UNDERSCORE} -o ${HOME}/build-out/project.bin
 cp output/${PROJECT_NAME_UNDERSCORE} ${HOME}/build-out/project.elf
