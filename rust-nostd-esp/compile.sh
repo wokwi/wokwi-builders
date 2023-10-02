a8a7889ef5950f4f60934e99e000168e3e6a1c5d
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-10-02 09:51:26 +0200

    fix: Remove underscored name

--- rust-nostd-esp/compile.sh
+++ rust-nostd-esp/compile.sh
@@ -25,7 +25,6 @@ esac
 cd ${PROJECT_NAME}
 mkdir -p output
 PROJECT_ROOT="${HOME}/${PROJECT_NAME}"
-PROJECT_NAME_UNDERSCORE=${PROJECT_NAME//-/_}
 
 if [ "$(find ${HOME}/build-in -name '*.rs')" ]; then
     cp ${HOME}/build-in/*.rs src
@@ -38,5 +37,5 @@ fi
 
 cargo audit
 cargo build --release --out-dir output -Z unstable-options
-python3 -m esptool --chip ${WOKWI_MCU} elf2image --flash_size 4MB ${PROJECT_ROOT}/output/${PROJECT_NAME_UNDERSCORE} -o ${HOME}/build-out/project.bin
-cp output/${PROJECT_NAME_UNDERSCORE} ${HOME}/build-out/project.elf
+python3 -m esptool --chip ${WOKWI_MCU} elf2image --flash_size 4MB ${PROJECT_ROOT}/output/${PROJECT_NAME} -o ${HOME}/build-out/project.bin
+cp output/${PROJECT_NAME} ${HOME}/build-out/project.elf
