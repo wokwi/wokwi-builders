8f04aa49e3703e99d14b50a0099ec8e0a51098b4
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-10-02 11:19:26 +0200

    fix: Use Wokwi MCU name without dashes

--- rust-nostd-esp/compile.sh
+++ rust-nostd-esp/compile.sh
@@ -25,6 +25,7 @@ esac
 cd ${PROJECT_NAME}
 mkdir -p output
 PROJECT_ROOT="${HOME}/${PROJECT_NAME}"
+WOKWI_MCU_NO_DASH="${WOKWI_MCU//-/}"
 
 if [ "$(find ${HOME}/build-in -name '*.rs')" ]; then
     cp ${HOME}/build-in/*.rs src
@@ -37,5 +38,5 @@ fi
 
 cargo audit
 cargo build --release --out-dir output -Z unstable-options
-espflash save-image --chip ${WOKWI_MCU} --flash-size 4mb ${PROJECT_ROOT}/output/${PROJECT_NAME} ${HOME}/build-out/project.bin
+espflash save-image --chip ${WOKWI_MCU_NO_DASH} --flash-size 4mb ${PROJECT_ROOT}/output/${PROJECT_NAME} ${HOME}/build-out/project.bin
 cp output/${PROJECT_NAME} ${HOME}/build-out/project.elf
