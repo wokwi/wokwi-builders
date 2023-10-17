5857773ad709062f13da2358ce4cfb25b3d9c36b
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-10-17 14:33:25 +0200

    fix: Fix timeouts

--- rust-nostd-avr/compile.sh
+++ rust-nostd-avr/compile.sh
@@ -30,9 +30,12 @@ if [ -f ${HOME}/build-in/Cargo.toml ]; then
     sed -i 's/^[[:space:]]*name[[:space:]]*=[[:space:]]*["'"'"']\([^"'"'"']*\)["'"'"']\([[:space:]]*\)$/\nname = "'${PROJECT_NAME}'"/' Cargo.toml
 fi
 
-rm -rf ${HOME}/.cargo/registry/index/* ${HOME}/.cargo/.package-cache
+rm -rf ~/.cargo/.package-cache
+echo "cargo clean"
 cargo clean
+echo "cargo audit"
 cargo audit
+echo "cargo build"
 cargo build --release --out-dir output -Z unstable-options
 avr-objcopy -R .eeprom -O ihex ./target/${MCU}/release/${PROJECT_NAME}.elf ${HOME}/build-out/project.hex
 cp ./target/${MCU}/release/${PROJECT_NAME}.elf ${HOME}/build-out/project.elf
